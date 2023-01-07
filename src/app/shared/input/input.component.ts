c17e95763845d7fd29f5d1346895c6ca8fb1ed44
snehal patel <snpatel.dev@gmail.com>
2023-01-06 23:14:15 -0500

    #6 resuable input, styling & more validations

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent {
+  @Input() label!: string;
+  @Input() control!: FormControl;
+  @Input() inputType!: string;
 
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
 }
